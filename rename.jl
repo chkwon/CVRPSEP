fac9f063f8d94eade017294b47a39997e51ee20b
Changhyun Kwon <chkwon@gmail.com>
2021-11-26 09:26:01 +0900

    rename: lowercases

--- rename.jl
+++ rename.jl
@@ -3,6 +3,10 @@ all_files = readdir()
 for (i, file) in enumerate(all_files)
     new_filename = lowercase(file)
     new_filename = replace(new_filename, "cpp" => "c")
-    run(`mv $file $new_filename`)
+    try
+        println("$file => $new_filename")
+        run(`git mv $file $new_filename`)
+    catch e 
+    end
     println("$file => $new_filename")
 end
